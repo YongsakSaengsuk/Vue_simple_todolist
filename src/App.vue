3f364577ac392a2e40834d081bfda0f72b7797bd
YonsakSaengsuk <yongsaksaengsuk@gmail.com>
2024-12-05 14:04:21 +0700

    Revert "create list from data"
    This reverts commit 4f1b46c3a58998037802676ab72247ac232f7f81.

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,33 @@
 <script>
 import Form from './components/Form.vue'
-import List from './components/List.vue'
 
-export default {
-  name: 'App',
-  data() {
-    return {
-      tasks: [
-        { id: 1, text: "Task 1" },
-        { id: 2, text: "Task 2" },
-        { id: 3, text: "Task 3" }
-      ]
+    export default {
+      name: 'App',
+      data() {
+        return {
+          tasks: [
+            { id: 1, text: "Task 1" },
+            { id: 2, text: "Task 2" },
+            { id: 3, text: "Task 3" }
+          ]
+        }
+      },
+      components: {
+        Form
+      },
+      methods: {
+        saveText(value) {
+          console.log(value)
+          this.tasks.push({ id: this.tasks.length + 1, text: value })
+        }
+      }
     }
-  },
-  components: {
-    Form,
-    List
-  },
-  methods: {
-    saveText(value) {
-      console.log(value)
-      this.tasks.push({ id: this.tasks.length + 1, text: value })
-    }
-  }
-}
 </script>
 
 <template>
   <div>
     <h1>TODO LIST</h1>
-    <Form :saveText="saveText" />
-    <ul>
-      <List v-for="tasks in tasks" :key="tasks.id" :taskname="tasks.text"/>
-    </ul>
+    <Form :saveText="saveText"/>
   </div>
 </template>
 
@@ -43,11 +38,9 @@ export default {
   will-change: filter;
   transition: filter 300ms;
 }
-
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
-
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
